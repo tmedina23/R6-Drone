5ef44cf6f5fb355eff55b60cfda196581c20637d
tmedina23 <tmedina23@bsmschool.org>
2023-03-27 16:28:24 -0500

    asdfghjkl

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, render_template, Response, request
 from time import *
+import cv2
 from adafruit_servokit import *
+
+
 
 kit = ServoKit(channels=16)
 
@@ -31,11 +34,33 @@ def left():
         frontright.throttle = 0.5
         frontleft.throttle = 0.5
 
+def get_frame():
+    #set video capture device with device number, variables above
+    camera = cv2.VideoCapture(0)
+    #camera width
+    w=854
+    #camera height
+    h=480
+    camera.set(3,w)
+    camera.set(4,h)
+    camera.set(cv2.CAP_PROP_FPS, 20)
+
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            break
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tobytes() + b'\r\n\r\n')
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed0():
+    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/stop_all')
 def stop():
