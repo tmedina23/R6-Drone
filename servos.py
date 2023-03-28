d9bee8f6c847dbe4ef46ecf7b8239aa7fb152817
tmedina23 <tmedina23@bsmschool.org>
2023-03-28 12:15:45 -0500

    dlkhsgjfdfs

--- servos.py
+++ servos.py
@@ -4,6 +4,12 @@ from time import *
 
 kit = ServoKit(channels=16)
 
+left = kit.continuous_servo[1]
+right = kit.continuous_servo[0]
+
+left.throttle = 0
+right.throttle = 0
+
 def test(servo_num):
         kit.continuous_servo[servo_num].throttle = -0.5
         sleep(1)
@@ -12,12 +18,12 @@ def test(servo_num):
         kit.continuous_servo[servo_num].throttle = 0
 
 def stopAll():
-        kit.continuous_servo[2].throttle = 0
-        kit.continuous_servo[1].throttle = 0
+        left.throttle = 0
+        right.throttle = 0
 
-def left1(servo_num):
-        kit.continuous_servo[servo_num].throttle = 0.5
+def left1():
+        left.throttle = 0.5
 
-left1(1)
-sleep(5)
+left1()
+sleep(3)
 stopAll()
